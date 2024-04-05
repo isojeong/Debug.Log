fc972c7af91d3651b75c1d7d14802b928c3754e8
isojeong <166107690+isojeong@users.noreply.github.com>
2024-04-05 16:38:35 +0900

    테스트

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello unity!");
+
+        int level = 5;
+        float strength = 15.5f;
+        string playerName = "skill";
+        bool isFullLevel = false;
+
+        Debug.Log("이름은?")
+        Debug.Log(playerName);
+        Debug.Log("나이는?")
+
+        Debug.Log("레벨은?")
+    }
+   
+        
+ }
